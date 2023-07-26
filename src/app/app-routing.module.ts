03bddb1f467ab15b0add10a4f50118c42ea2a2d9
IDGS-802-20001500 <mauricioisraelfer@gmail.com>
2023-07-25 19:43:11 -0600

    Se actualizo el proyecto por completo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './modulos/login/login/login.component';
+import { ProductoComponent } from './modulos/producto/producto/producto.component';
+import { RegistroComponent } from './modulos/login/registro/registro.component';
+import { FinanzasComponent } from './modulos/finanzas/finanzas/finanzas.component';
+import { CompraComponent } from './modulos/compra/compra/compra.component';
+import { InventarioComponent } from './modulos/inventario/inventario/inventario.component';
+import { PedidoComponent } from './modulos/pedido/pedido/pedido.component';
+import { ProveedorComponent } from './modulos/proveedor/proveedor/proveedor.component';
+import { UsuarioComponent } from './modulos/usuario/usuario/usuario.component';
+import { VentaComponent } from './modulos/venta/venta/venta.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: "/home", pathMatch:'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'producto',component: ProductoComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'login/registro', component: RegistroComponent},
+  {path: 'compras', component: CompraComponent },
+  {path: 'finanzas', component: FinanzasComponent },
+  {path: 'inventario', component: InventarioComponent },
+  {path: 'pedidos', component: PedidoComponent },
+  {path: 'proveedores', component: ProveedorComponent },
+  {path: 'usuarios', component: UsuarioComponent },
+  {path: 'ventas', component: VentaComponent },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,46 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { InventarioComponent } from './modulos/inventario/inventario/inventario.component';
+import { MateriaPrimaComponent } from './modulos/materia_prima/materia-prima/materia-prima.component';
+import { RecetaComponent } from './modulos/receta/receta/receta.component';
+import { ProveedorComponent } from './modulos/proveedor/proveedor/proveedor.component';
+import { PedidoComponent } from './modulos/pedido/pedido/pedido.component';
+import { CompraComponent } from './modulos/compra/compra/compra.component';
+import { VentaComponent } from './modulos/venta/venta/venta.component';
+import { UsuarioComponent } from './modulos/usuario/usuario/usuario.component';
+import { FinanzasComponent } from './modulos/finanzas/finanzas/finanzas.component';
+import { LoginComponent } from './modulos/login/login/login.component';
+import { RegistroComponent } from './modulos/login/registro/registro.component';
+import { ProductoComponent } from './modulos/producto/producto/producto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    InventarioComponent,
+    MateriaPrimaComponent,
+    RecetaComponent,
+    ProveedorComponent,
+    PedidoComponent,
+    CompraComponent,
+    VentaComponent,
+    UsuarioComponent,
+    FinanzasComponent,
+    LoginComponent,
+    RegistroComponent,
+    ProductoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
